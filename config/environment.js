2cb4d6bce7f13f521305ebd26be1326149d4bc87
RDegnen <ross@biobright.com>
2020-01-24 16:40:22 -0500

    good shit

--- config/environment.js
+++ config/environment.js
@@ -47,5 +47,7 @@ module.exports = function(environment) {
     // here you can enable a production-specific feature
   }
 
+  ENV.MAPBOX_ACCESS_TOKEN = 'pk.eyJ1IjoiZWwtc3R1bXAiLCJhIjoiY2p4cXczaHN3MDJsazNncnNkM2J0cmttaiJ9.biXsJyAoZuq9U8ZrngxJmA'
+  
   return ENV;
 };
